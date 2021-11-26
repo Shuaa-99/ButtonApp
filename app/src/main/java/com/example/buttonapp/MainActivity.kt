2c2229a2ec5091022e0917b119d473307b2345e2
Shua'a <senshen2018@gmail.com>
2021-11-26 16:26:15 +0300

    Add Snackbar

--- app/src/main/java/com/example/buttonapp/MainActivity.kt
+++ app/src/main/java/com/example/buttonapp/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var myLayout = findViewById<ConstraintLayout>(R.id.clMain)
         counter_txt = findViewById(R.id.counter_txt)
       button_inc = findViewById(R.id.button_inc)
         button_dec = findViewById(R.id.button_dec)
@@ -21,10 +24,13 @@ class MainActivity : AppCompatActivity() {
         button_inc.setOnClickListener {
             counterValue += 1
             counter_txt.text = counterValue.toString()
+            Snackbar.make(myLayout, "Increased by one", Snackbar.LENGTH_LONG).show()
         }
         button_dec.setOnClickListener {
             counterValue -= 1
             counter_txt.text = counterValue.toString()
+            Snackbar.make(myLayout, "Decreased by one", Snackbar.LENGTH_LONG).show()
+
         }
     }
 }
